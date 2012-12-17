cb75ed98262075259ba682e4b01f0d1ea5977743
biallo <bialloworld@gmail.com>
2012-12-17 18:18:36 +0800

    素材列表筛选、广告列表静态页面

--- Runtime/Cache/2400900ae0ee2fb32613d828fbecbdbe.php
+++ Runtime/Cache/2400900ae0ee2fb32613d828fbecbdbe.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<?php if (!defined('THINK_PATH')) exit();?><!doctype html>
 <html>
 	<head>
 		<meta charset="utf-8">
@@ -7,7 +7,59 @@
 		<link rel="shortcut icon" href="__PUBLIC__/img/favicon.ico" type="image/x-icon">
 		<link rel="stylesheet" href="__PUBLIC__/css/also.css" type="text/css">
 	<body>
-		<include file="./Tpl/Comm/menu.html"/>
+				<div class="menu">
+			<div class="logo"><a href="#"><img src="__PUBLIC__/img/logo.png" alt="Adnets 广告管理系统"></a></div>
+			<div class="navbar">
+				<a href="<?php echo U('index/ad');?>" class="nav 
+				<?php if(($m==ad)): ?>n_on<?php endif; ?>
+				">
+					<span class="nav_arrow"></span>
+					<span class="nav_ad" alt="广告"></span>
+					<p>广告</p>
+				</a>
+				
+				<a href="<?php echo U('index/loca');?>" class="nav 
+				<?php if(($m==loca)): ?>n_on<?php endif; ?>
+				">
+					<span class="nav_arrow"></span>
+					<span class="nav_loca" alt="广告位"></span>
+					<p>广告位</p>
+				</a>
+
+				<a href="<?php echo u('index/mater');?>" class="nav 
+				<?php if(($m==mater)): ?>n_on<?php endif; ?>
+				">
+					<span class="nav_arrow"></span>
+					<span class="nav_mat" alt="素材库"></span>
+					<p>素材库</p>
+				</a>
+				<span class="bline" ></span>
+				<a href="#" class="nav">
+					<span class="nav_arrow"></span>
+					<span class="nav_ord" alt="客户和订单"></span>
+					<p>客户和订单</p>
+				</a>
+
+				<a href="#" class="nav">
+					<span class="nav_arrow"></span>
+					<span class="nav_data" alt="数据统计"></span>
+					<p>数据统计</p>
+				</a>
+				<span class="bline" ></span>
+				<a href="#" class="nav">
+					<span class="ad_on_num">9</span>
+					<span class="nav_arrow"></span>
+					<span class="nav_info" alt="通知中心"></span>
+					<p>通知中心</p>
+				</a>
+			</div>
+
+			<div class="footer">
+				<p><a href="#">admin</a></p>
+				<p><a href="#">系统设置</a></p>
+				<p><a href="#">退出</a></p>
+			</div>
+		</div>
 		<body>
 		<div class="main">
 			<div class="board">
@@ -80,8 +132,8 @@
 								<a href="#">查看统计</a>
 							</div>
 						</div>
-					</div>
 
+					</div>
 					<span class="filter">没有符合条件的广告</span>
 					<div class="pages_box">
 						<div id="loading_pgs"><span class="load_pgs"></span></div>
